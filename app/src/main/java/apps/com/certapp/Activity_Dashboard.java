067f5a852e95f1bee3e9e7be39b897597c1ad4bc
Shivani Nawathe <nawatheshivani@gmail.com>
2018-08-01 19:31:05 +0530

    next commit

---
+++ app/src/main/java/apps/com/certapp/Activity_Dashboard.java
@@ -0,0 +1,13 @@
+package apps.com.certapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Activity_Dashboard extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- app/src/main/java/apps/com/certapp/Activity_Main.java
+++ app/src/main/java/apps/com/certapp/Activity_Main.java
@@ -1,8 +1,10 @@
 package apps.com.certapp;
 
+import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,6 +13,8 @@ public class Activity_Main extends AppCompatActivity {
     EditText editTextUsername,editTextPassword;
     TextInputLayout textInputLayoutUsername,textInputLayoutPaswword;
     Button buttonLogin;
+
+    public String stringUsername = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,28 @@ public class Activity_Main extends AppCompatActivity {
         textInputLayoutUsername = (TextInputLayout)findViewById(R.id.til_Username);
         textInputLayoutPaswword = (TextInputLayout)findViewById(R.id.til_Pwd);
         buttonLogin = (Button)findViewById(R.id.btn_login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int cnt = 0;
+
+                stringUsername = editTextUsername.getText().toString();
+
+                if (editTextPassword.getText().length() != 4){
+                    textInputLayoutPaswword.setError("Enter Valid Password");
+                    cnt ++;
+                }
+                if (editTextUsername.getText().length() < 2){
+                    textInputLayoutUsername.setError("Enter Valid Username");
+                    cnt ++;
+                }
+
+                if (cnt == 0){
+                    Intent i = new Intent(Activity_Main.this, Activity_Dashboard.class);
+                    startActivity(i);
+                }
+            }
+        });
     }
 }
